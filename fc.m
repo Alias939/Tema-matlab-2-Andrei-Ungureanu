ddf677c06ed2ee5c248a29641a6037f336e18f29
Andrei Ungureanu <trekker_andrei12@yahoo.com>
2016-12-13 23:42:32 +0200

    Update fc.m

--- fc.m
+++ fc.m
@@ -1,10 +1,5 @@
 function [ functia ] = fc( t,f0,fi,A,T )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
 
-
-
-%functia = sawtooth(2*pi*f0*t + fi, 0.5);
 l = length(t);
 for i = 1:l
 if mod(t(i),T) < T/5 
